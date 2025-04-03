4f32b0c1776a0cf8860b9af22260cb30a285238a
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-04-03 15:06:05 +0200

    refactor: use angle utility for the opposite angle

--- lab3/repulsive.lua
+++ lab3/repulsive.lua
@@ -1,23 +1,12 @@
+local angle_utils = require("angle_utils")
 local ps = require("sensor_schema")
 local repulsive = {}
 
 
 function repulsive.get_vector(robot)
 	local vector = ps.from_sensors(robot.proximity, 1, 0.1, repulsive.module_force)
-	return {angle=invert_angle(vector.angle), length=vector.length}
+	return {angle=angle_utils.opposite(vector.angle), length=vector.length}
 end
-
-function invert_angle(angle)
-	local opposite_angle = angle - math.pi
-	if opposite_angle > math.pi then
-	  opposite_angle = opposite_angle - (2 * math.pi)
-	elseif opposite_angle < -math.pi then
-	  opposite_angle = opposite_angle + (2 * math.pi)
-	end
-	return opposite_angle
-end
-
-
 
 function repulsive.module_force(distance)
 	if (distance >= 0.10) then
