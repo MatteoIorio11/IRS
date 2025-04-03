8241a374d5de54aa0810b88f84151f50c08b106e
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-04-03 15:05:52 +0200

    feat: use angle utility for random walking

--- lab3/random_force.lua
+++ lab3/random_force.lua
@@ -1,23 +1,11 @@
+local angle_utils = require("angle_utils")
 local random = {}
 
-local last_angle = 0
 
 function random.get_vector(robot)
-    local angle = random.normalise(math.random(0, 360) * (math.pi / 180))
+    local angle = angle_utils.normalise(math.random(0, 360) * (math.pi / 180))
     local vector = {angle=angle, length=math.random(0.0, 1.0)}
     return vector
-end
-
-
-
-
-function random.normalise(angle)
-	if angle > math.pi then
-        angle = angle - (2 * math.pi)
-	elseif angle < -math.pi then
-        angle = angle + (2 * math.pi)
-	end
-    return angle
 end
 
 return random
