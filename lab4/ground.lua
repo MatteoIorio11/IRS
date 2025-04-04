157f8434ce6a05f334cfeb6047dfe85261c245af
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-04-04 17:31:45 +0200

    doc: add lua doc

--- lab4/ground.lua
+++ lab4/ground.lua
@@ -1,13 +1,17 @@
 local ground = {}
 
+---comment Read the lowest color tonality detected. If 0 then is black otherwise is white.
+---@param robot any
+---@return integer color this function returns the color with the lowest tonality.
 function ground.detect_ground_color(robot)
-    local sensors = robot.motor_ground
-    local color = math.huge
-    for i=1, #sensors do
-        local ground_color = sensors[i].value
-        color = math.min(ground_color, color)
-    end
-    return color
+	local sensors = robot.motor_ground
+	local color = math.huge
+	for i = 1, #sensors do
+		local ground_color = sensors[i].value
+		color = math.min(ground_color, color)
+	end
+	return color
 end
 
 return ground
+
