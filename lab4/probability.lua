48b9918e1e4875ea311847d319a1dfba100752e6
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-04-04 17:31:50 +0200

    doc: add lua doc

--- lab4/probability.lua
+++ lab4/probability.lua
@@ -5,31 +5,35 @@ local ground = require("ground")
 local PS_max = 0.99
 local PW_min = 0.005
 
+---comment Return the probability for the robot to stop. The formula is: Ps = min(PSmax, S + alpha * N)
 function probability.should_stop(robot, S, alpha)
-    local total_stopped = signal.count_stopped(robot)
-    return probability.apply(math.min(PS_max, (S + alpha * total_stopped)))
+	local total_stopped = signal.count_stopped(robot)
+	return probability.apply(math.min(PS_max, (S + alpha * total_stopped)))
 end
 
+---comment Return the probability for the robot to stop. The formula is: Pw = max(PWmin, W - beta * N)
 function probability.should_walk(robot, W, beta)
-    local total_stopped = signal.count_stopped(robot)
-    return probability.apply(math.max(PW_min, (W - beta * total_stopped)))
+	local total_stopped = signal.count_stopped(robot)
+	return probability.apply(math.max(PW_min, (W - beta * total_stopped)))
 end
 
+---comment Return the probability for the robot to stop. The formula is: Pw = max(PWmin, W - beta * N + Dw)
 function probability.should_walk_with_spot(robot, W, beta)
-    local total_stopped = signal.count_stopped(robot)
-    local color = ground.detect_ground_color(robot)
-    return probability.apply(math.max(PW_min, (W - beta * total_stopped) + color))
+	local total_stopped = signal.count_stopped(robot)
+	local color = ground.detect_ground_color(robot)
+	return probability.apply(math.max(PW_min, (W - beta * total_stopped) + color))
 end
 
+---comment Return the probability for the robot to stop. The formula is: Ps = min(PSmax, S + alpha * N - Ds)
 function probability.should_stop_with_spot(robot, S, alpha)
-    local total_stopped = signal.count_stopped(robot)
-    local color = ground.detect_ground_color(robot)
-    return probability.apply(math.min(PS_max, (S + alpha * total_stopped) - color))
+	local total_stopped = signal.count_stopped(robot)
+	local color = ground.detect_ground_color(robot)
+	return probability.apply(math.min(PS_max, (S + alpha * total_stopped) - color))
 end
 
 function probability.apply(prob)
-    local t = robot.random.uniform()
-    return t <= prob
+	local t = robot.random.uniform()
+	return t <= prob
 end
 
 return probability
