d2a2fdc9d2e3c8e6d40e31db9182e52d340d8a40
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-04-04 15:48:29 +0200

    feat: add new probability for walking

--- lab4/probability.lua
+++ lab4/probability.lua
@@ -2,13 +2,21 @@ local probability = {}
 local PS_max = 0.99
 local PW_min = 0.005
 
-function probability.should_stop(robots, S, alpha, stopped)
+function probability.should_stop(S, alpha, stopped)
     return probability.apply(math.min(PS_max, (S + alpha * stopped)))
 end
 
-function probability.should_walk(robots, W, beta, walking)
+function probability.should_walk(W, beta, walking)
     return probability.apply(math.max(PW_min, (W - beta * walking)))
-  end
+end
+
+function probability.should_walk_with_spot(W, beta, walking, ground)
+    return probability.apply(math.max(PW_min, (W - beta * walking) + ground))
+end
+
+function probability.should_stop_with_spot(S, alpha, stopped, ground)
+    return probability.apply(math.min(PS_max, (S + alpha * stopped) + ground))
+end
 
 function probability.apply(prob)
     local t = robot.random.uniform()
