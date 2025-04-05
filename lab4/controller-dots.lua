ce79351cb09626f31fce5aa169f5306de8371ab4
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-04-05 15:07:49 +0200

    refactor: use states annotations

--- lab4/controller-dots.lua
+++ lab4/controller-dots.lua
@@ -27,6 +27,9 @@ function step()
     end
 end
 
+
+-- walking functionality, by using this function the robot checks if there are some other robots nearby, then if it
+-- detects other robot, it will avoid them, otherwise it will perform a random walk.
 function walking()
     if avoid_logic.sense(robot) then
         avoid_logic.callback(robot)
@@ -35,15 +38,22 @@ function walking()
     end
 end
 
+-- this function handle all the operations the robot has to do when it comes to stop:
+-- 1: signal the other robot that it will stop
+-- 2: set the leds color to green
+-- 3: stop the robot
 function handle_stop()
-    currrent_state = 1
+    currrent_state = states.STOPPED
     signal.stop_moving(robot)
     color.go_green(robot)
-    robot.wheels.set_velocity(0, 0)
+    walk.set_velocity(robot, 0, 0)
 end
 
+-- this function handle all the operations the robot has to do when it comes to start walkingl:
+-- 1: signal all the other robots that it will start walking
+-- 2: turn off all the leds
 function handle_walk()
-    currrent_state = 2
+    currrent_state = states.WALKING
     signal.start_moving(robot)
     color.go_black(robot)
 end
