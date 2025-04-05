c3e3b20d608c23f8f85145ebb0fff2e522ae7823
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-04-05 15:13:00 +0200

    fix: remove bug

--- lab4/signal.lua
+++ lab4/signal.lua
@@ -2,39 +2,40 @@ local signal = {}
 
 local CHANNEL = 1
 local STOP = 1
-local WALKING = 0 
+local WALKING = 0
 local MAX_RANGE = 30
 
 -- this function signals the other robots the fact that 'this' has started to walk.
 function signal.start_moving(robot)
-    robot.range_and_bearing.set_data(CHANNEL,WALKING)
+	robot.range_and_bearing.set_data(CHANNEL, WALKING)
 end
 
 -- this function signals the other robots the fact that 'this' has stopped.
 function signal.stop_moving(robot)
-    robot.range_and_bearing.set_data(CHANNEL,STOP)
+	robot.range_and_bearing.set_data(CHANNEL, STOP)
 end
 
 -- this function counts the number of robot that are curretly stopped.
 function signal.count_stopped(robot)
-    return signal.count_status(robot, STOPPED)
+	return signal.count_status(robot, STOPPED)
 end
 
 -- this function counts the number of robot that are currently walking.
-function signal.count_walking(robot).
-    return signal.count_status(robot, WALKING)
+function signal.count_walking(robot)
+	return signal.count_status(robot, WALKING)
 end
 
 -- this function counts the number of robot that are in a given status.
 function signal.count_status(robot, status)
-    local robots = 0
-    for i=1, #robot.range_and_bearing do
-        local neighbour = robot.range_and_bearing[i]
-        if neighbour.range < MAX_RANGE and neighbour.data[CHANNEL] == status then
-            robots = robots + 1
-        end
-    end
-    return robots
+	local robots = 0
+	for i = 1, #robot.range_and_bearing do
+		local neighbour = robot.range_and_bearing[i]
+		if neighbour.range < MAX_RANGE and neighbour.data[CHANNEL] == status then
+			robots = robots + 1
+		end
+	end
+	return robots
 end
 
 return signal
+
