aa2b4361d011a98f31c8173867c3cc3fd0407b57
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-04-06 12:33:39 +0200

    refactor: use general methods also for versions with spots

--- lab4/probability.lua
+++ lab4/probability.lua
@@ -19,16 +19,14 @@ end
 
 ---comment Return the probability for the robot to stop. The formula is: Pw = max(PWmin, W - beta * N + Dw)
 function probability.should_walk_with_spot(robot, W, beta)
-	local total_stopped = signal.count_stopped(robot)
 	local color = ground.detect_ground_color(robot)
-	return probability.apply(math.max(PW_min, (W - beta * total_stopped) + color))
+	return probability.should_walk(W + color, beta)
 end
 
 ---comment Return the probability for the robot to stop. The formula is: Ps = min(PSmax, S + alpha * N - Ds)
 function probability.should_stop_with_spot(robot, S, alpha)
-	local total_stopped = signal.count_stopped(robot)
 	local color = ground.detect_ground_color(robot)
-	return probability.apply(math.min(PS_max, (S + alpha * total_stopped) - color))
+	return probabilit.should_stop(S - color, alpha)
 end
 
 function probability.apply(prob)
