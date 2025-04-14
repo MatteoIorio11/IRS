d42dee1743ff1e5facf54a801e8a7a77b08b44a4
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-04-14 17:50:40 +0200

    doc: add doc

--- lab0/Phototaxi/move_random_logic.lua
+++ lab0/Phototaxi/move_random_logic.lua
@@ -1,6 +1,7 @@
 local generalmodule = require("general")
 local moverandommodule = {}
 
+-- Make the robot walk randomly.
 function moverandommodule.callback(robot)
 	local left_v = robot.random.uniform(0, generalmodule.MAX_VELOCITY)
 	local right_v = robot.random.uniform(0, generalmodule.MAX_VELOCITY)
