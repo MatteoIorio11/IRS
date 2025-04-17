e9b56a2063e99cade89c830cada6ff135321f97a
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-04-17 18:07:46 +0200

    refactor: rename file, change function signature and add doc

---
+++ lab3/random_vector.lua
@@ -0,0 +1,11 @@
+local angle_utils = require("angle_utils")
+local random = {}
+
+-- Generate a random vector
+function random.generate_random_vector(robot)
+    local angle = angle_utils.generate_random_angle() -- get the angle
+    local vector = {angle=angle, length=math.random(0.0, 1.0)} -- get the length
+    return vector
+end
+
+return random
