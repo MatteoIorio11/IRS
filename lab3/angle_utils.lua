49ce9cf17d87afd00553339d07c7d69ae9427330
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-04-17 18:08:31 +0200

    feat: add new function for generating a random angle

--- lab3/angle_utils.lua
+++ lab3/angle_utils.lua
@@ -1,5 +1,6 @@
 local angle_utils = {}
 
+-- Normalise the input angle in the range [0, 2pi]
 function angle_utils.normalise(angle)
         if angle > math.pi then
                 angle = angle - (2 * math.pi)
@@ -9,9 +10,15 @@ function angle_utils.normalise(angle)
         return angle
 end
 
+-- Get the opposite of the input angle
 function angle_utils.opposite(angle)
 	local opposite_angle = angle - math.pi
-        return angle_utils.normalise(opposite_angle)
+        return angle_utils.normalise(opposite_angle) -- add safety
+end
+
+-- Generate a random angle in the range [0, 2pi]
+function angle_utils.generate_random_angle()
+        return  angle_utils.normalise(math.random(0, 360) * (math.pi / 180))
 end
 
 return angle_utils
